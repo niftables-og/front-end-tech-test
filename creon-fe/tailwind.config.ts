01526075935778af72f1e1985ef558f51160933d
USER <obichilaka@gmail.com>
2024-05-03 20:19:00 +0100

    updates on tailwind.config.ts

--- creon-fe/tailwind.config.ts
+++ creon-fe/tailwind.config.ts
@@ -2,18 +2,68 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: "#000",
+        gray: {
+          "100": "rgba(13, 19, 28, 0.7)",
+          "200": "rgba(19, 23, 29, 0.6)",
+        },
+        white: "#fff",
+        "grey-line": "#13171d",
+        darkslategray: {
+          "100": "#454a4f",
+          "200": "rgba(69, 74, 79, 0.09)",
+        },
+        blue: "#3d8bff",
+        purple: "#ab23ff",
+        "grey-text": "#46505f",
+      },
+      spacing: {},
+      fontFamily: {
+        satoshi: "Satoshi",
+        "monument-extended": "'Monument Extended'",
+      },
+      borderRadius: {
+        "81xl": "100px",
+        "12xs": "1px",
       },
     },
+    fontSize: {
+      lg: "18px",
+      "19xl": "38px",
+      "11xl": "30px",
+      "4xl": "23px",
+      "3xl": "22px",
+      xs: "12px",
+      "49xl": "68px",
+      "35xl": "54px",
+      "22xl": "41px",
+      "3xs": "10px",
+      base: "16px",
+      inherit: "inherit",
+    },
+    screens: {
+      mq1825: {
+        raw: "screen and (max-width: 1825px)",
+      },
+      mq1350: {
+        raw: "screen and (max-width: 1350px)",
+      },
+      mq925: {
+        raw: "screen and (max-width: 925px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
 };
